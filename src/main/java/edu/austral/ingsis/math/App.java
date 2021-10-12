4d1132f56e82bbc78a506817c25a3e4ea9fe2964
tomasberretta <tomas.berretta@ing.austral.edu.ar>
2021-10-12 11:53:44 -0300

    Deleted unused classes

--- src/main/java/edu/austral/ingsis/math/App.java
+++
@@ -1,7 +0,0 @@
-package edu.austral.ingsis.math;
-
-public class App {
-    public static void main(String[] args) {
-    }
-
-}
--- src/main/java/edu/austral/ingsis/math/FunctionPrinter.java
+++
@@ -1,4 +0,0 @@
-package edu.austral.ingsis.math;
-
-public interface FunctionPrinter {
-}
